4ece857115b4e95f0d096fcf736d7418f3a767d4
Pavel Kuzmin <Virus191288@gmail.com>
2023-04-12 16:45:33 +0500

    update

--- src/runtime/components/lib/OpenApiTabs.vue
+++ src/runtime/components/lib/OpenApiTabs.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="border-b border-gray-200 p-2 border rounded border-gray-300">
-    <div class="container mx-auto text-sm font-medium text-center border-b">
+    <div class="container mx-auto text-sm font-medium text-center border-b" v-if="tabNames.length > 1">
       <div class="flex">
         <div v-for="(tab, index) in tabNames" :key="index"
              class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-blue-300 dark:hover:text-gray-300 font-bold"
