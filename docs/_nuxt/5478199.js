4e581a53924bf63e2bf4a3edc806b0d6d3261230
Pavel Kuzmin <Virus191288@gmail.com>
2023-08-16 12:24:09 +0500

    update

--- docs/_nuxt/5478199.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{981:function(n,e,r){"use strict";r.r(e);var t={props:{html:{type:String,required:!0}},render:function(n){return[n("div",{innerHTML:this.html})]}},d=r(3),component=Object(d.a)(t,undefined,undefined,!1,null,null,null);e.default=component.exports}}]);
---
+++ docs/_nuxt/e7160b9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[35],{980:function(e,n,t){"use strict";t.r(n);var l={name:"OpenApiTableNested"},c=t(3),component=Object(c.a)(l,(function(){return(0,this._self._c)("div",[this._t("default")],2)}),[],!1,null,null,null);n.default=component.exports}}]);
