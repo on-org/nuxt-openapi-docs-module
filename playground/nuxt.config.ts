93398bbe5891a429d320477b0f582594ee8ca454
Pavel Kuzmin <Virus191288@gmail.com>
2024-05-06 12:27:51 +0500

    update docs

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -172,8 +172,8 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      readme: marked(fs.readFileSync(path.resolve(process.cwd(), './README.md'), 'utf-8')),
-      changelog: marked(fs.readFileSync(path.resolve(process.cwd(), './changelog.md'), 'utf-8'))
+      readme: marked(fs.readFileSync(path.resolve(__dirname, '../README.md'), 'utf-8')),
+      changelog: marked(fs.readFileSync(path.resolve(__dirname, '../changelog.md'), 'utf-8'))
     }
   }
 })
